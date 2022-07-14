92eff44cb43d07a7012598bb4a02d1f98a56276f
Denys Kovika <d.v.kovika@gmail.com>
2022-07-14 13:21:07 +0300

    ver.1

--- src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
+++ src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
@@ -10,8 +10,15 @@ public class SumOfEvenNumbers {
 
     public static int sum(int[] array){
 
+        int sum1 = 0;
+        if (array == null || array.length ==0) return 0;
+            else { for (int arr:array) {
+                if (arr%2==0) sum1 +=arr;
+        }
+        }
+            return sum1;
         //put your code here
 
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
     }
 }
